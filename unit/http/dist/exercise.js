275ae0b63fd9132832ed88796b9ebee15cbffa68
ArielWu0203 <lucky020372@gmail.com>
2020-04-13 21:30:04 +0800

    http week

--- unit/http/dist/exercise.js
+++ unit/http/dist/exercise.js
@@ -3,8 +3,15 @@ $(document).ready(function() {
     event.preventDefault()
     // Step 9 and step 10 code goes here
 
-    $.get('./step5')
+    $.get('./step5', {
+      fname: $('#ajax-form input[name=fName]').val(),
+      lname: $('#ajax-form input[name=lName]').val(),
+    }, (data) => {
+      $('#ajax-output').html(data);
+    })    
 
     // Step 11 code goes here
+    
+    
   })
 });
